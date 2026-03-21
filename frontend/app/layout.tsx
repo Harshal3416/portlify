bb0998a6624aacf795eb1d8fe2f94108bf9c8d19
harshal3416 <harshalbelamkar3416@gmail.com>
2026-03-21 18:39:49 +0530

    initial setup with clerk

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,6 +6,7 @@ import { FooterComponent } from "./components/ui/footercomponent";
 import QueryProvider from "@/providers/QueryProvider";
 import { SiteProvider } from "./context/siteContext";
 import { ToastProvider } from "./context/ToastContext";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import 'bootstrap/dist/css/bootstrap.min.css';  // Single line fix!
 
 const geistSans = Geist({
@@ -38,7 +39,22 @@ export default function RootLayout({
           <SiteProvider>
             <QueryProvider>
               <ToastProvider>
-                {children}
+                <ClerkProvider>
+                  <header className="flex justify-end items-center p-4 gap-4 h-16">
+                    <Show when="signed-out">
+                      <SignInButton />
+                      <SignUpButton>
+                        <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                          Sign Up
+                        </button>
+                      </SignUpButton>
+                    </Show>
+                    <Show when="signed-in">
+                      <UserButton />
+                    </Show>
+                  </header>
+                  {children}
+                </ClerkProvider>
               </ToastProvider>
             </QueryProvider>
             <FooterComponent></FooterComponent>
